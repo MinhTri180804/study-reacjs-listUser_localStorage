fd7bc23b3a05d3a3363cd3d6065ad97a64236875
unknown <nguyenminhtri1808t@gmail.com>
2023-05-11 01:43:59 +0700

    test deploy vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
       <div>
@@ -21,12 +22,17 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+
+      <p className="decs-title">
+        Hello everyone my name is Tri
       </p>
     </>
   )
